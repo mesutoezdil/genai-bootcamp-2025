8fbbb3b5aececdb7f440c90878d6cfca01d93351
Mesut Oezdil <114185853+mesutoezdil@users.noreply.github.com>
2025-04-04 12:40:54 +0000

    new

---
+++ week_3_opea/OPEA-chatqna/modules/_common/tags/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, staging, prod)"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name"
+}
